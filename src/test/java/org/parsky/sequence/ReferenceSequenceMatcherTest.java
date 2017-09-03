e5b0ace27ebbfb51573db4b4ef700cd122eccbb7
Joao Melo <jmelo@lyncode.com>
2017-09-03 20:16:55 +0100

    Fixing broken test

--- src/test/java/org/parsky/sequence/ReferenceSequenceMatcherTest.java
+++ src/test/java/org/parsky/sequence/ReferenceSequenceMatcherTest.java
@@ -3,19 +3,16 @@ package org.parsky.sequence;
 import org.junit.Test;
 import org.parsky.sequence.model.SequenceMatcherResult;
 
-import java.util.concurrent.atomic.AtomicReference;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.parsky.sequence.SequentTestUtils.request;
 
 public class ReferenceSequenceMatcherTest {
-    private final AtomicReference<StringSequenceMatcher> reference = new AtomicReference<>();
-    private ReferenceSequenceMatcher<StringSequenceMatcher> referenceSequenceMatcher = new ReferenceSequenceMatcher<StringSequenceMatcher>();
+    private ReferenceSequenceMatcher<StringSequenceMatcher> referenceSequenceMatcher = new ReferenceSequenceMatcher<>();
 
     @Test
     public void match() throws Exception {
-        reference.set(new StringSequenceMatcher("test"));
+        referenceSequenceMatcher.assign(new StringSequenceMatcher("test"));
 
         SequenceMatcherResult result = referenceSequenceMatcher.matches(request("test"));
 
